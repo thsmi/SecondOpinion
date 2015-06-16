943d5acef03d66630dd6a2125f670284a37b9915
Thomas Schmid <schmid-thomas@gmx.net>
2015-06-17 00:34:36 +0200

    Make apikey an optional paramert Whitespaces and ApiKey

--- secondopinion@tschmid.net/content/virustotal.file.js
+++ secondopinion@tschmid.net/content/virustotal.file.js
@@ -11,74 +11,77 @@ if (!net.tschmid.secondopinion)
 (function() {
   function SecondOpinionFiles() {}
 
-  SecondOpinionFiles.prototype = {			
+  SecondOpinionFiles.prototype = {      
   
     // Scans the attachment urls passed.  
-    uploadFile : function(name, chunks, onCompleted, onProgress) {
+    uploadFile : function(name, chunks, onCompleted, onProgress, apikey) {
 
       let filename = name;
 
       if (this.getSettings().isNameObfuscated())
         filename = Math.random().toString(36).slice(2);
       
-	  var blob = new File(chunks, filename, { type: "application/octet-stream"} );
-	  
-      var formData = new FormData();	
-	  formData.append("file", blob);
-	  formData.append("apikey",this.getSettings().getApiKey());
-	  
+      var blob = new File(chunks, filename, { type: "application/octet-stream"} );
+    
+      if (!apikey)
+        apikey = this.getSettings().getApiKey();    
+    
+      var formData = new FormData();  
+      formData.append("file", blob);
+      formData.append("apikey",this.getSettings().getApiKey());
+    
       var request = new XMLHttpRequest();
-	  
-	  var self = this;
+    
+      var self = this;
       request.onload = function(event) { 
-	    self.getRequests().remove(name);
-	    onCompleted(name, this); 
-	  };
-	  
-	  request.upload.onprogress = function(event) {onProgress(name, event)};
+      self.getRequests().remove(name);
+      onCompleted(name, this); 
+    };
+    
+    request.upload.onprogress = function(event) {onProgress(name, event)};
       request.open("POST","https://www.virustotal.com/vtapi/v2/file/scan");
-	  request.send(formData);   
-	  
-	  this.getRequests().add(name,request);
+      request.send(formData);   
+    
+      this.getRequests().add(name,request);
     },
 
-	getFileReport : function(name, checksum, callback) {
+    getFileReport : function(name, checksum, callback) {
   
       let formData = new FormData();
       formData.append("resource",checksum);
       formData.append("apikey",this.getSettings().getApiKey());
 
       let request = new XMLHttpRequest();
-	
-	  let self = this;
+  
+      let self = this;
       request.onload = function(e) {
-	    self.getRequests().remove(checksum);
-	    callback(name, checksum, this)
+        self.getRequests().remove(checksum);
+        callback(name, checksum, this)
       };
-	
+  
       request.open("POST","https://www.virustotal.com/vtapi/v2/file/report");
       request.send(formData);
-	
-	  this.getRequests().add(checksum, request);
+  
+      this.getRequests().add(checksum, request);
     }, 
 
-    // Api short cuts ...	
+    // Api short cuts ... 
     getSettings : function () {
-	  if (!net.tschmid.secondopinion.settings)
-	    throw "Failed to import settings";
-	
-	  return net.tschmid.secondopinion.settings;
-	},
-	
+      if (!net.tschmid.secondopinion.settings)
+        throw "Failed to import settings";
+  
+      return net.tschmid.secondopinion.settings;
+    },
+  
     getRequests : function() {
-	  if (!net.tschmid.secondopinion.requests)
-	    throw "Failed to import requests";
-	
-	  return net.tschmid.secondopinion.requests;	
-	},
+      if (!net.tschmid.secondopinion.requests)
+        throw "Failed to import requests";
+  
+      return net.tschmid.secondopinion.requests;  
+    },
   }
   
   // Export an instance to the global Scope  
-  net.tschmid.secondopinion.files = new SecondOpinionFiles();  	
+  net.tschmid.secondopinion.files = new SecondOpinionFiles();   
   
 }());  
--- secondopinion@tschmid.net/content/virustotal.url.js
+++ secondopinion@tschmid.net/content/virustotal.url.js
@@ -17,66 +17,72 @@ if (!net.tschmid.secondopinion)
     ADDRESS_URL_REPORT  : "https://www.virustotal.com/vtapi/v2/url/report",
   
     /**
-	   * Requests an Report at virus total for the given URL.
-	   *
-	   *  @param{String|String[]} url
-	   *    an url or and array of urls as string
+     * Requests an Report at virus total for the given URL.
+     *
+     *  @param{String|String[]} url
+     *    an url or and array of urls as string
      *  
      *  @param{callback(url, request)} callback
      *    the callback which should be invoked when the reponse is received.
-     *    the url parameter is passed transparently to the callback.	 
-	   **/	 
-    getUrlReport : function(url, callback) {
+     *    the url parameter is passed transparently to the callback.   
+     *
+     *  @optional @param{String} apikey
+     *    the api key, in case it is null or omitted it will query the settings for a key.
+     **/   
+    getUrlReport : function(url, callback, apikey) {
     
-	    let resource = url;
-	  
-	    if (Array.isArray(url)) {	  
-	  	
-	  	  if (!url.length)
-	  		  return;
-		
-		    resource = "";
-	  
-	  	  url.forEach(function(item) {
-	  		  resource += item +"\n"
-	      } );
-	    }
+      let resource = url;
+    
+      if (Array.isArray(url)) {   
+      
+        if (!url.length)
+          return;
+    
+        resource = "";
+    
+        url.forEach(function(item) {
+          resource += item +"\n"
+        } );
+      }
+    
+      if (!apikey)
+        apikey = this.getSettings().getApiKey();
     
       let formData = new FormData();
-	  
-      formData.append("resource",resource);
-	    formData.append("scan", 1);
-      formData.append("apikey",this.getSettings().getApiKey());
+    
+      formData.append("resource", resource);
+      formData.append("scan", 1);
+      formData.append("apikey", apikey);
     
       let request = new XMLHttpRequest();
-	  
-	    let self = this;
+    
+      let self = this;
       request.onload = function(e) {
-	      self.getRequests().remove(url);
-	      callback(url, this)
+        self.getRequests().remove(url);
+        callback(url, this)
       };
-	  
+    
       request.open("POST",this.ADDRESS_URL_REPORT);
       request.send(formData);
-	  
-	    this.getRequests().add(url, request);
-    }, 	 
+    
+      this.getRequests().add(url, request);
+    },   
 
-	  getSettings : function () {
-	    if (!net.tschmid.secondopinion.settings)
-	      throw "Failed to import settings";
-	
-	    return net.tschmid.secondopinion.settings;
-	  },
-	
+    getSettings : function () {
+      if (!net.tschmid.secondopinion.settings)
+        throw "Failed to import settings";
+  
+      return net.tschmid.secondopinion.settings;
+    },
+  
     getRequests : function() {
-	    if (!net.tschmid.secondopinion.requests)
-	      throw "Failed to import requests";
-	
-	    return net.tschmid.secondopinion.requests;	
-	  },	
+      if (!net.tschmid.secondopinion.requests)
+        throw "Failed to import requests";
+  
+      return net.tschmid.secondopinion.requests;  
+    },  
   }
   
   // Export an instance to the global Scope  
-  net.tschmid.secondopinion.url = new SecondOpinionUrl();  	  
+  net.tschmid.secondopinion.url = new SecondOpinionUrl();     
 }());
